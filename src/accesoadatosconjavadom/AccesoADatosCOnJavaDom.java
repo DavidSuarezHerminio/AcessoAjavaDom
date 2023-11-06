9db0f9a52b7562276b3cccd647087feffe884789
DavidSuarezHerminio <148563032+DavidSuarezHerminio@users.noreply.github.com>
2023-11-06 19:44:26 +0100

    commit inicial

---
+++ src/accesoadatosconjavadom/AccesoADatosCOnJavaDom.java
@@ -0,0 +1,24 @@
+
+package accesoadatosconjavadom;
+
+import Clases.AccesoDOM;
+import java.io.File;
+
+
+public class AccesoADatosCOnJavaDom {
+
+    
+    public static void main(String[] args) {
+    AccesoDOM a=new AccesoDOM();
+    
+File f=new File("Libros.xml");//necesitamos Libros.xml en la ruta correcta
+
+a.abrirXMLaDOM(f);
+a.recorreDOMyMuestra();
+a.insertarLibroEnDOM("Giuliete", "David suarez", "22-4-20023");
+a.deleteNode("Giuliete");
+a.guardarDOMcomoArchivo("LibrosDeDOM.xml");
+}
+    }
+    
+
